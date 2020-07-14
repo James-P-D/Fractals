b6f87515404b81a0426bb03857d9ecec9d5ef10a
James Dorrington <jdorrington@hotmail.com>
2020-07-14 14:31:33 +0100

    Moving mandlebrot to separate file

--- src/Fractals/Fractals/Fractals.py
+++ src/Fractals/Fractals/Fractals.py
@@ -5,6 +5,7 @@ from Constants import *
 from UIControls import *
 import datetime
 import colorsys
+from Mandlebrot import draw_mandlebrot
 
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
@@ -119,33 +120,6 @@ def game_loop():
 ###############################################
 # mandlebrot_button_clicked()
 ###############################################
-def rgb_conv(i): 
-    color = 255 * np.array(colorsys.hsv_to_rgb(i / 255.0, 1.0, 0.5)) 
-    return tuple(color.astype(int)) 
-  
-def mandelbrot(x, y, iterations): 
-    c0 = complex(x, y) 
-    c = 0
-    for i in range(1, iterations): 
-        if abs(c) > 2: 
-            return rgb_conv(i) 
-        c = c * c + c0 
-    return (0, 0, 0) 
-
-def draw_mandlebrot():
-    x_pixel_diff = (max_x - min_x) / CANVAS_WIDTH
-    y_pixel_diff = (max_y - min_y) / CANVAS_HEIGHT
-
-    start_time = datetime.datetime.now()
-    for x in range(CANVAS_WIDTH):  
-        # displaying the progress as percentage 
-        #print("%.2f %%" % (x / CANVAS_WIDTH * 100.0))  
-        for y in range(CANVAS_HEIGHT): 
-            (r, g, b) = mandelbrot((x * x_pixel_diff) + min_x, (y * y_pixel_diff) + min_y, MANDLEBROT_ITERATIONS)
-            screen.set_at((x, y), (r, g, b, 255))
-    end_time = datetime.datetime.now()
-    diff = end_time - start_time
-    print(diff.seconds, "s")
 
 def mandlebrot_button_clicked():    
     global current_fractal
@@ -160,7 +134,7 @@ def mandlebrot_button_clicked():
     global min_x, min_y, max_x, max_y
     (min_x, min_y, max_x, max_y) = (MANDLEBROT_MIN_X, MANDLEBROT_MIN_Y, MANDLEBROT_MAX_X, MANDLEBROT_MAX_Y)
     
-    draw_mandlebrot()
+    draw_mandlebrot(min_x, min_y, max_x, max_y, screen)
    
 ###############################################
 # zoom_button_clicked()
@@ -198,7 +172,7 @@ def zoom_button_clicked():
 
     print((min_x, min_y, max_x, max_y))
 
-    draw_mandlebrot()
+    draw_mandlebrot(min_x, min_y, max_x, max_y, screen)
 
     zoom_set = False
 
