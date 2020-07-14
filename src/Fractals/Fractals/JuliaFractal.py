2985888f543b6abc49fe78c0ac1271ca492757b1
James Dorrington <jdorrington@hotmail.com>
2020-07-14 15:35:09 +0100

    Tidying

--- src/Fractals/Fractals/JuliaFractal.py
+++ src/Fractals/Fractals/JuliaFractal.py
@@ -15,9 +15,10 @@ def draw_julia_fractal(min_x, min_y, max_x, max_y, screen):
             zx = (x * x_pixel_diff) + min_x
             zy = (y * y_pixel_diff) + min_y
             i = maxIter 
-            while zx*zx + zy*zy < 4 and i > 1: 
-                tmp = zx*zx - zy*zy + cX 
-                zy,zx = 2.0*zx*zy + cY, tmp 
+            while ((((zx * zx) + (zy * zy)) < 4) and (i > 1)): 
+                temp = (zx * zx) - (zy * zy) + cX 
+                zy = (2.0 * zx * zy) + cY
+                zx = temp 
                 i -= 1
   
             screen.set_at((x, y), (i << 21) + (i << 10) + i*8)
